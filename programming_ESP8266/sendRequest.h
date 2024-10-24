d805b02ed1adec11fd4dc7678f2054a8de343867
juareza21 <161495617+juareza21@users.noreply.github.com>
2024-10-24 00:49:01 -0700

    Update sendRequest.h

--- programming_ESP8266/sendRequest.h
+++ programming_ESP8266/sendRequest.h
@@ -2,23 +2,16 @@
 #define SENDREQUEST_H
 
 #include <Arduino.h>
+#include <ArduinoJson.h>
+#include <Adafruit_Sensor.h>
 #include <DHT.h>
 
-void connectionDetails()
-{
-  // Details of the connection
-  Serial.print("Connected to ");
-  Serial.println(WiFi.SSID());              // Tell us what network we're connected to
-  Serial.print("IP address:\t");
-  Serial.println(WiFi.localIP());           // Send the IP address of the ESP8266 to the computer
-  Serial.print("Channel:\t");
-  Serial.println(WiFi.channel());          
-  Serial.print("RSSI:\t");
-  Serial.println(WiFi.RSSI());   
-  Serial.print("DNS IP:\t");
-  Serial.println(WiFi.dnsIP(1));            // get the DNS IP Address    
-  Serial.print("Gateway IP:\t");
-  Serial.println(WiFi.gatewayIP()); 
-  Serial.println("---------------------");    
-}
+extern DHT dht;
+
+void transmitData(float temperature, float humidity, const String& timestamp);
+
+float readtemp();
+float readhum();
+String gettime(const String& timeZone);
+void connectionDetails();
 #endif
