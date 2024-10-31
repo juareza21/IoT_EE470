ec98e9fcd04ffbc03d665c6829337e2995c01b8b
juareza21 <161495617+juareza21@users.noreply.github.com>
2024-10-30 23:14:23 -0700

    Create connect.h

---
+++ RGB/connect.h
@@ -0,0 +1,15 @@
+#ifndef CONNECT_H
+#define CONNECT_H
+
+#include <Arduino.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void connectionDetails();
+
+String readresults(String);
+
+int* readRGB(String url);
+
+#endif
